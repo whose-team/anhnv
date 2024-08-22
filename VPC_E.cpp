9089b2ed9ce59f7242b949e368f399e7eae83a61
anhnv-dell <anht2k19@qq.com>
2024-08-23 02:28:22 +0700

    Accept VPC_E

--- VPC_E.cpp
+++ VPC_E.cpp
@@ -12,11 +12,13 @@ class Solusion{
         {
             cin >>inputN >> inputP >>inputQ;
             inputA.clear();
-            int temp;
-            for (int i=0;i<inputN;i++)
+            long long temp;
+            sum=0;
+            for (long long i=0;i<inputN;i++)
             {
                 cin>>temp;
                 inputA.push_back(temp);
+                sum+=temp;
             }
                 
         }
@@ -26,40 +28,79 @@ class Solusion{
             valueStatus.clear();
             for (int i=0;i<inputN;i++)
                 valueStatus.push_back(-1);
-            int currentValue = inputP;
-            int tempValue=0;
-            int index=0;
-            while(1){
-                if(index>inputN) break;
-                if(index==inputN) index=0;
-                if(inputA.at(index)<0) {
-                    tempValue = -min(currentValue, abs(inputA.at(index)));   
-                }else{tempValue=inputA.at(index);}
-                currentValue+=tempValue;
-                outResult++;
+            long long currentValue = inputP;
+            outResult=-1;
+            for(int i=0;i<inputN;i++){
+                currentValue+=inputA[i];
+                valueStatus[i] = currentValue;
                 if(currentValue<=inputQ){
+                    outResult=i+1;
                     break;
                 }
-                if(valueStatus[index]!=-1&&valueStatus[index]<=currentValue){
-                    outResult=-1;
-                    break;
-                }
-                index++;
             }
-            cout<<"AAAAA: "<<endl;
+            if(outResult==-1 && sum<0){
+                long long min=100000000000;
+                long long index=0;
+                int temp=0;
+                if (abs(sum)>inputQ)
+                {
+                    for(int i=0;i<inputN;i++)
+                    {
+                        temp = int(valueStatus[i]%abs(sum)>inputQ);
+                        int current = long(valueStatus[i]/abs(sum))+temp;
+                        if( min>current){
+                            index=i+1;
+                            min=current;
+                        }
+                    }
+                    outResult = min*inputN+index;
+                        
+                }
+                else if (abs(sum)==inputQ)
+                {
+                    for(int i=0;i<inputN;i++)
+                    {
+                        temp = int(valueStatus[i]%abs(sum)==0);
+                        if( min>long(valueStatus[i]/abs(sum))-temp){
+                            index=i+1;
+                            min=long(valueStatus[i]/abs(sum))-temp;
+                        }
+                    
+                    }
+                    outResult = min*inputN+index;
+
+                }else{
+                    for(int i=0;i<inputN;i++)
+                    {
+                        temp = int((valueStatus[i]-inputQ)%abs(sum)!=0);
+                        int current = long((valueStatus[i]-inputQ)/abs(sum))+temp;
+                        if( min>current){
+                            index=i+1;
+                            min=current;
+                        }
+                    
+                    }
+                    outResult = min*inputN+index;
+
+                }
+                
+            }
+            
+            // cout<<"AAAAA: "<<endl;
             cout<<outResult<<endl;
 
         }
     public:
-    int inputN, inputP, inputQ;
-    int outResult;
-    vector<int> inputA;
-    vector<int> valueStatus;
+    long long inputN, inputP, inputQ;
+    long long sum=0;
+    long long outResult;
+    vector<long long> inputA;
+    vector<long long> valueStatus;
         
 };
 
 int main(){
-    int testNum;
+    long long testNum;
 
     if (!MULTI_TEST ){
         Solusion solution;
@@ -67,7 +108,7 @@ int main(){
         solution.process();
     }else{
         cin >> testNum;
-        for (int test=0; test<testNum;test++){
+        for (long long test=0; test<testNum;test++){
             Solusion solution;
             solution.readData();
             solution.process();
