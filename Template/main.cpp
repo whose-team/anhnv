b712bbb7e707744f94f79aaa7d01fe1e557ed9c0
anhnv208 <201326090@qq.com>
2024-09-03 17:12:45 +0700

    add BFS

--- Template/main.cpp
+++ Template/main.cpp
@@ -2,31 +2,33 @@
 #include "DFS_Visit.h"
 #include <bits/stdc++.h>
 int N;
-void customReadGraph() {
 
-    cin>>N;
-    cin>>startPoint;
-    cin>>endPoint;
-    for(int u=1; u<=N; u++) {
-        int v=1;
-        while(v!=0) {
-            cin>>v;
-            if(v==0) break;
+void customReadGraph() {
+    cin >> N;
+    cin >> startPoint;
+    cin >> endPoint;
+    for (int u = 1; u <= N; u++) {
+        int v = 1;
+        while (v != 0) {
+            cin >> v;
+            if (v == 0) break;
             addCanhUV(u, v);
         }
     }
     trace[startPoint] = startPoint;
 }
+
 void testDFS() {
-    freopen("../dfsVisit.txt","r",stdin);
+    freopen("../dfsVisit.txt", "r",stdin);
     initGraph();
     customReadGraph();
     DFSVisit(startPoint);
-    for(int i=1;i<=N;i++)
-        if(trace[i]!=0) std::cout<<i<<" ";
-    cout<<"\n";
+    for (int i = 1; i <= N; i++)
+        if (trace[i] != 0) std::cout << i << " ";
+    cout << "\n";
     printPath(startPoint, endPoint);
 }
+
 int main() {
     testDFS();
 
---
+++ Template/notes.md
@@ -0,0 +1,8 @@
+# DFS
+- Đường đi có độ dài dài nhất (2 lần DFS)
+- Đường đi có thứ tự từ điển bé nhất 
+
+
+# BFS
+- ĐƯờng đi ngắn nhất
+- 
